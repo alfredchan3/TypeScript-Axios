f96a2aa71dfa96498b1da39f7dfcd683dd675967
alfred <brilychen@gmail.com>
2019-06-19 00:17:16 +0800

    feat(handle request body data): Add handle request body data function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
@@ -8,12 +9,17 @@ function axios(config: AxiosRequestConfig): void {
 }
 
 function processConfig(config: AxiosRequestConfig): void {
-  config.url = transfromURL(config)
+  config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 
-function transfromURL(config: AxiosRequestConfig): string {
+function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default axios
